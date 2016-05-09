36055cc2a7e6b24037d5d62634b571ee6c31fd53
Nadia Barbosa <nadiabarbosa@me.com>
2016-05-09 17:03:21 -0400

    Add user model test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  fixtures :users
+
+  it 'should have an email' do
+    expect(users(:leif_ericksson).email).to eq 'leif@explorer.com'
+  end
+
+  it 'should have a first name' do
+    expect(users(:leif_ericksson).first_name).to eq 'Leif'
+  end
+
+  it 'should have a last name' do
+    expect(users(:leif_ericksson).last_name).to eq 'Ericksson'
+  end
 end
