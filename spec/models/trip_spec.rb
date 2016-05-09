526a21d1d9247a3f8eb22c4c99cf9aeedaccdb6e
Nadia Barbosa <nadiabarbosa@me.com>
2016-05-09 16:42:44 -0400

    Add trip model test

--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Trip, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  fixtures :trips, :users
+
+  it 'should have a name' do
+    expect(trips(:iceland).name).to eq 'Iceland'
+  end
+
+  it 'should have a description' do
+    expect(trips(:iceland).description).to eq 'More of like a staycation'
+  end
+
+  it 'should belong to a user' do
+    expect(trips(:iceland).user.id).to eq users(:leif_ericksson).id
+  end
 end
