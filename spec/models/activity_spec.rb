b917659f8802cfd19c0b6cd57a2b2877161e45af
Nadia Barbosa <nadiabarbosa@me.com>
2016-05-09 17:00:15 -0400

    Add activity model test

--- spec/models/activity_spec.rb
+++ spec/models/activity_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Activity, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  fixtures :activities, :trips
+
+  it 'should have a name' do
+    expect(activities(:fishing).name).to eq 'Sunrise Fishing Trip'
+  end
+
+  it 'should belong to a trip' do
+    expect(activities(:fishing).trip_id).to eq trips(:iceland).id
+  end
+
+  it 'should have a date' do
+    expect(activities(:fishing).date).to eq "2016-01-03 06:00:00"
+  end
+
+  it 'should have a description' do
+    expect(activities(:fishing).description).to eq "Looking to catch some cod and salmon"
+  end
 end
