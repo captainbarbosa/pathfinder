734e523a35ac0bb29e5032e51c88392481f54424
Nadia Barbosa <nadiabarbosa@me.com>
2016-05-10 13:34:58 -0400

    Add validations for activities

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,3 +1,5 @@
 class Activity < ActiveRecord::Base
   belongs_to :trip
+
+  validates :trip_id, :name, :date, presence: true
 end
--- spec/models/activity_spec.rb
+++ spec/models/activity_spec.rb
@@ -18,4 +18,17 @@ RSpec.describe Activity, type: :model do
   it 'should have a description' do
     expect(activities(:fishing).description).to eq "Looking to catch some cod and salmon"
   end
+
+  it 'should require trip_id, name, and date for creation' do
+    @trip = trips(:iceland)
+    @@activity_without_trip_id = Activity.create(name: 'Fishing', date: '2016-05-10 17:32:02')
+    @activity_without_name = Activity.create(trip_id: @trip.id, date: '2016-05-10 17:32:02')
+    @activity_without_date = Activity.create(trip_id: @trip.id, name: 'Whale Watching')
+    @valid_activity = Activity.create(trip_id: @trip.id, name: 'Whale Watching', date: '2016-05-10 17:32:02')
+
+    expect(@activity_without_trip_id.valid?).to be false
+    expect(@activity_without_name.valid?).to be false
+    expect(@activity_without_date.valid?).to be false
+    expect(@valid_activity.valid?).to be true
+  end
 end
