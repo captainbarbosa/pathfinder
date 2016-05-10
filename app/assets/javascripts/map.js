b139044172f47e63c071261a9581b9939fe18288
Nadia Barbosa <nadiabarbosa@me.com>
2016-05-10 16:10:26 -0400

    Add basemap to trips#show

---
+++ app/assets/javascripts/map.js
@@ -0,0 +1,13 @@
+// Start doing things only when DOM is ready
+document.addEventListener("DOMContentLoaded", function(event) {
+
+  mapboxgl.accessToken = 'pk.eyJ1IjoibmJiMTI4MDUiLCJhIjoiMUlFVjZWVSJ9.jeNyiaRq8MCHlXSlGQZIHA';
+
+  // Initialize basemap
+  var map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/nbb12805/cio1vac0f0007ahm4q0xl9d34',
+      center: [ -77.03656196594238,  38.89769637048576],
+      zoom: 10
+  });
+});
