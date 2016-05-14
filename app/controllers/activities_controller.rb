c62df422166e3a734c761e526e53474f2050070e
Nadia Barbosa <nadiabarbosa@me.com>
2016-05-14 16:11:00 -0400

    Rearrange authorization/routing for home page

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,4 +1,6 @@
 class ActivitiesController < ApplicationController
+    before_action :authenticate_user!
+
     def index
       @activities = Activity.all
     end
--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,4 +1,5 @@
 class TripsController < ApplicationController
+  before_action :authenticate_user!
 
   def index
     @trips = current_user.trips
