ffaa867d5615560bbdf8995db79f3ea1c98f0ea0
Nadia Barbosa <nadiabarbosa@me.com>
2016-05-17 13:38:54 -0400

    Add date range helper to trips index

--- app/helpers/trips_helper.rb
+++ app/helpers/trips_helper.rb
@@ -2,4 +2,11 @@ module TripsHelper
   def activities_geojson(trip)
     trip.activities.where("address is NOT NULL").map { |e| e.to_geojson }.to_json
   end
+
+  def date_range(trip)
+    dates = trip.activities.pluck(:date)
+    max_date = dates.max_by { |x| x }
+    min_date = dates.min_by { |x| x }
+    { start_date: max_date, end_date: min_date }
+  end
 end
